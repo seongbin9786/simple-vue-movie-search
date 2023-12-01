c499772d9d704090335b3e8799c3c82aa0053d75
junly21 <jaejoon0721@ajou.ac.kr>
2023-12-01 15:37:58 +0900

    INIT

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h2>App.vue</h2>
+  <h3>{{ count }}</h3>
+  <HelloWorld />
+</template>
