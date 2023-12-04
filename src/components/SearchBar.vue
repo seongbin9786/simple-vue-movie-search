c7e906e956c18eeb65ae036ea94e05214ecb288b
Seongbin Kim <seongbin9786@gmail.com>
2023-12-04 23:04:14 +0900

    style: 로고 및 입력창 스타일링

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,6 +7,7 @@ defineEmits(["submitTitle"]);
         <h2 class="search-bar__title">Search Movie</h2>
         <input
             class="search-bar__input"
+            placeholder="엔터 키로 검색..."
             @keydown.enter="$emit('submitTitle', ($event.target as HTMLInputElement).value)"
         />
     </div>
