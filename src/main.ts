98ba7b3444c8506111857f17e6e5ac25329a4067
Seongbin Kim <seongbin9786@gmail.com>
2023-12-04 11:18:32 +0900

    chore: 불필요한 pinia, vue-router 제거

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import './style.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
 
-const pinia = createPinia()
 const app = createApp(App)
-app.use(pinia)
 app.mount('#app')
