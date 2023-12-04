f1d4c49ab90ca1ad684bf0e69ba153e192454267
Seongbin Kim <seongbin9786@gmail.com>
2023-12-04 23:46:43 +0900

    refactor: searchMovieList 함수 이름 개선

--- src/pages/MovieSearchPage.vue
+++ src/pages/MovieSearchPage.vue
@@ -7,7 +7,7 @@ import { Movie, fetchMovieList } from "../api/api";
 // 초기값은 null, api 반환 값은 빈 배열일 수 있음.
 const data = ref<Movie[] | null>(null);
 
-const searchMovie = async (title: string) => {
+const searchMovieList = async (title: string) => {
     try {
         data.value = await fetchMovieList(title);
     } catch (e) {
@@ -19,6 +19,6 @@ const searchMovie = async (title: string) => {
 </script>
 
 <template>
-    <SearchBar @submit-title="searchMovie" />
+    <SearchBar @submit-title="searchMovieList" />
     <MovieList :movies="data" />
 </template>
